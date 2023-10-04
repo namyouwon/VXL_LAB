42f8914932fd33fb5249961de54ca3aeb3619c84
Nam <hoainam15112002@gmail.com>
2023-10-04 19:31:50 +0700

    ex4 update

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -91,24 +91,72 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 1);
-  int count=2;
-    while (1)
+  enum TrafficLightState {
+      RED1_GREEN2,
+  	RED1_YELLOW2,
+  	GREEN1_RED2,
+  	YELLOW1_RED2,
+    };
+  // Initialize the current state of the traffic light
+  int counter = 3;
+  int light = 5;
+  enum TrafficLightState currentState = RED1_GREEN2;
+  enum TrafficLightState nextState = currentState;
+  while (1)
     {
-  	  if(count<=0){
-  		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-  		  HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
-  		  count=2;
+  	  display7SEG(light);
+  	  switch (currentState) {
+  	    case RED1_GREEN2:
+  	    	turnonTraffigLights(RED1);
+  	    	turnonTraffigLights(GREEN2);
+  	    	counter--;
+  	    	light--;
+  	    	if(counter <= 0)
+  	    	{
+  	    		nextState = RED1_YELLOW2;
+  	    		counter = 2;
+  	    	}
+  	    	break;
+  	    case RED1_YELLOW2:
+  	    	turnonTraffigLights(RED1);
+  	    	turnonTraffigLights(YELLOW2);
+  	    	counter--;
+  	    	light--;
+  	    	if(counter <= 0)
+  	    	{
+  	    		nextState = GREEN1_RED2;
+  		    	counter = 3;
+  		    	light = 3;
+  	    	}
+  	    	break;
+  	    case GREEN1_RED2:
+  	    	turnonTraffigLights(GREEN1);
+  	    	turnonTraffigLights(RED2);
+  	    	counter--;
+  	    	light--;
+  	    	if(counter <= 0)
+  	    	{
+  	    		nextState = YELLOW1_RED2;
+  		    	counter = 2;
+  		    	light = 2;
+  	    	}
+  	    	break;
+  	    case YELLOW1_RED2:
+  	    	turnonTraffigLights(YELLOW1);
+  	    	turnonTraffigLights(RED2);
+  	    	counter--;
+  	    	light--;
+  	    	if(counter <= 0)
+  	    	{
+  	    		nextState = RED1_GREEN2;
+  		    	counter = 3;
+  		    	light = 5;
+  	    	}
+  	    	break;
   	  }
-  	  count--;
+  	  currentState = nextState;
   	  HAL_Delay(1000);
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
     }
-  /* USER CODE END 3 */
-}
 
 /**
   * @brief System Clock Configuration
